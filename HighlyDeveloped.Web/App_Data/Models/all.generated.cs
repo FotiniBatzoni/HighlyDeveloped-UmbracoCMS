04be307ca2b652a5adf171a0e48513b6b0b2f2cb
fotini <f.batzoni@gmail.com>
2023-03-22 19:54:22 +0200

    Adding References to Main Page

--- HighlyDeveloped.Web/App_Data/Models/all.generated.cs
+++ HighlyDeveloped.Web/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "2b836a075d3f38ac")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.24")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
