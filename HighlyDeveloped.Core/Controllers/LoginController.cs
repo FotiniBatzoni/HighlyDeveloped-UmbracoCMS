c9e19b44ef0211a0b2ae56699fc02b9091e41fe3
fotini <f.batzoni@gmail.com>
2023-04-08 11:41:46 +0300

    Adding LoginViewModel

---
+++ HighlyDeveloped.Core/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Umbraco.Web.Mvc;
+
+namespace HighlyDeveloped.Core.Controllers
+{
+    /// <summary>
+    /// Login process
+    /// </summary>
+    public class LoginController : SurfaceController
+    {
+        public const string PARTIAL_VIEW_FOLDER="~/Views/Partials/Login";
+        public ActionResult RenderLogin()
+        {
+            var vm = LoginViewModel();
+            vm.RedirectUrl = HttpContext.Request.Url.AbsolutePath;
+            return PartialView(PARTIAL_VIEW_FOLDER + "Login.cshtml", vm);
+        }
+    }
+}
---
+++ HighlyDeveloped.Core/ViewModel/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighlyDeveloped.Core.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Display(Name ="Username")]
+        [Required]
+        public string UserName { get; set; }
+
+        [Display(Name = "Password")]
+        [Required]
+        public string Password { get; set; }
+        public string RedirectUrl { get; set; }
+    }
+}
