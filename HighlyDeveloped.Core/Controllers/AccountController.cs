734c1e2209e3d171a24e5e9c031407156990d841
fotini <f.batzoni@gmail.com>
2023-04-11 14:52:36 +0300

    Adding HandlePasswordChange

--- HighlyDeveloped.Core/Controllers/AccountController.cs
+++ HighlyDeveloped.Core/Controllers/AccountController.cs
@@ -73,5 +73,51 @@ namespace HighlyDeveloped.Core.Controllers
 
             return RedirectToCurrentUmbracoPage();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HandlePasswordChange(AccountViewModel vm)
+        {
+            //Is the model valid
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("Error", "There has been a problem");
+                return CurrentUmbracoPage();
+            }
+
+            //Is there a member
+            if (!Umbraco.MemberIsLoggedOn() || Membership.GetUser() == null)
+            {
+                ModelState.AddModelError("Error", "You are not logged on");
+                return CurrentUmbracoPage();
+            }
+
+            var member = Services.MemberService.GetByUsername(Membership.GetUser().UserName);
+            if (member == null)
+            {
+                ModelState.AddModelError("Error", "You are not logged on");
+                return CurrentUmbracoPage();
+            }
+
+            try
+            {
+                Services.MemberService.SavePassword(member, vm.Password);
+            }
+            catch  (MembershipPasswordException ex)
+            {
+
+                ModelState.AddModelError("Error", "There is a problem with your password");
+                return CurrentUmbracoPage();
+            }
+           
+        
+
+            Services.MemberService.Save(member);
+
+            //Thanks
+            TempData["status"] = "Updated Password";
+
+            return RedirectToCurrentUmbracoPage();
+        }
     }
 }
